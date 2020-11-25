7c2b7b724144d408f6555cf58a7ed8c7e8976f0e
AmaaniGoose <amanullah1707@gmail.com>
2020-11-25 14:11:14 +0530

    Integrated Nitish Changes

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('images/banner1.jpg');
+		$(".banner-image").backstretch('images/681016.jpg');
 		
 		// Fixed header
 		//-----------------------------------------------
